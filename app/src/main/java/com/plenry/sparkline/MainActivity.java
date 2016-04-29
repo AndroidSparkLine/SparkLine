f79b710e9ce4224264286441fc5f5a78ef791bfa
XiaoguangMo <nnnomm@gmail.com>
2016-04-29 12:53:17 -0700

    Prototype
    Apr 29

---
+++ app/src/main/java/com/plenry/sparkline/MainActivity.java
@@ -0,0 +1,13 @@
+package com.plenry.sparkline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
